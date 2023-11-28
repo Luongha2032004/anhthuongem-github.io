3556cea7b60825040996c51c84a1b3ce46028485
Luongha2032004 <125166379+Luongha2032004@users.noreply.github.com>
2023-11-28 14:25:57 +0700

    Add files via upload

---
+++ baitap-5.8.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <iomanip>
+using namespace std;
+class Hvuong{
+	private:
+		int dodaicanh;
+	public:
+		void nhaphv(){
+			cout<<"nhap vao canh cua hinh vuong: ";
+			cin<<dodaicanh;
+		}
+		void xuathv(){
+			cout<<"do dai canh cua hinh vuong: "
+		}
+		double dientichhv(){
+			return dodaicanh*dodaicanh;
+		}
+		double chuvihv(){
+			return dodaicanh*4						
+	};
+class CHUNHAT : public Hvuong {
+	private:
+		int dodaicanh2;
+	public:
+		void nhap_cn(){
+			cout<<"nhap do dai canh thu 2: ";
+			cin>>dodaicanh2;
+		}
+		void xuat_cn(){
+			cout<<"do dai canh thu 2: "	;
+		}
+		double chuvicn(){
+			return (dodaicanh+dodaicanhthu2)*2;
+		}
+		double dientichcn(){
+			return dodaicanh*dodaicanhthu2;	
+};	
+class quanly{
+	public:
+		void nhap(){
+		void xuat(){
+				
+			
+};
+int main() 
+{
+		
+	return 0;
+}
